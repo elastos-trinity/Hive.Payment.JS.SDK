c4881b5eff1a4572de2c9b5c085ea64fb0232d02
Fred Chen <chentj070921@yeah.net>
2022-07-29 08:49:55 +0800

    CU-394kc8f - Update the contract address of mainnet.

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export class Config {
     /** MainNet */
     static CONTRACT_URI = 'https://api.' + Config.BASE_API + '/eth';
     static CONTRACT_RPC = {20: Config.CONTRACT_URI}
-    static HIVE_NODE_ADDRESS = '0x0E7EE480dE19E5BB766bD790774B315b02bfD4e1';
+    static HIVE_NODE_ADDRESS = '0x59E9f4ff80f3B3A4810b5264EB713DC04F9DFC31';
 
     /** TestNet */
     static CONTRACT_TEST_URI = 'https://api-testnet.' + Config.BASE_API + '/eth';
